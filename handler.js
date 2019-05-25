f98cdc7745b31803381c539d33a886cae6b0af85
deveshkharve <devesh.kharve93@gmail.com>
2019-05-25 14:35:20 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -39,15 +39,4 @@ module.exports.mailer = async (event) => {
       message: `Go Serverless v1.0! Your function executed successfully! with status ${res}`,
     }, null, 2),
   };
-  // }
-  // ).catch( err=>{
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({
-  //       message: `Go Serverless v1.0! Your function executed successfully! with status false`,
-  //     }, null, 2),
-  //   };
-  // })
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
